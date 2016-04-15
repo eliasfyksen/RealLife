6d7a6318a07e1a4ab82ffe047dcb399919fe9453
eliasfyksen <elias.fyksen@spartanstudios.net>
2016-04-15 23:39:46 +0200

    fok this shit

---
+++ Assets/Scripts/Character/CameraVertical.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraVertical : MonoBehaviour {
+
+	public Vector2 maxRotation = new Vector2(0,0);
+
+	void Update () {
+
+		float turningValue = -Input.GetAxis ("Mouse Y") * Time.deltaTime * 100;
+		transform.Rotate (new Vector3 (turningValue, 0, 0));
+
+		if (transform.localRotation.x < maxRotation.x || transform.localRotation.x > maxRotation.y) {
+			transform.Rotate (new Vector3 (-turningValue, 0, 0));
+		}
+	}
+}
---
+++ Assets/Scripts/Character/CharacterControllerScript.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof (CharacterController))]
+public class CharacterControllerScript : MonoBehaviour {
+	
+	public float mass = 1, drag = 0.1f, jump = 1;
+	public Vector2 speed = new Vector2 (1, 1), acceleration = new Vector2 (1, 1), airAcceleration = new Vector2 (1, 1);
+	public Animator anim;
+
+	CharacterController character;
+	Vector3 velocity, movement;
+
+	void Start () {
+		character = GetComponent<CharacterController> ();
+		velocity = new Vector3 (0, 0, 0);
+		movement = new Vector3 (0, 0, 0);
+	}
+	
+	void Update () {
+		transform.Rotate (new Vector3 (0,Input.GetAxis("Mouse X") * Time.deltaTime * 100,0));
+
+		velocity.y += mass * Physics.gravity.y * Time.deltaTime;
+//		velocity -= velocity * drag * Time.deltaTime;
+
+		if (character.isGrounded) {
+			velocity.y = -0.01f;
+			movement.z = Mathf.Lerp (movement.z, Input.GetAxis ("Vertical") * speed.y, Time.deltaTime * acceleration.y);
+			movement.x = Mathf.Lerp (movement.x, Input.GetAxis ("Horizontal") * speed.x, Time.deltaTime * acceleration.x);
+			if (Input.GetButtonDown ("Jump")) {
+				velocity.y = jump;
+			}
+		} else {
+			movement.z = Mathf.Lerp (movement.z, Input.GetAxis("Vertical") * speed.y, Time.deltaTime * airAcceleration.y);
+			movement.x = Mathf.Lerp (movement.x, Input.GetAxis("Horizontal") * speed.x, Time.deltaTime * airAcceleration.x);
+		}
+
+		if (character.isGrounded && (Input.GetButton ("Vertical") || Input.GetButton ("Horizontal"))) {
+			anim.SetBool ("walk", true);
+		} else {
+			anim.SetBool ("walk", false);
+		}
+
+
+		Vector3 finalMovement = ((transform.rotation * movement) * Time.deltaTime);
+		Vector3 finalVelocity = velocity * Time.deltaTime;
+	
+		character.Move(finalVelocity + finalMovement);
+	}
+}
